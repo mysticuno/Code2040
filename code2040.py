bc6137ef147b666b935a00a5034101c2fbacebd0
Carlos Henriquez <carlosh@mit.edu>
2016-11-01 16:23:09 -0400

    Step 2: Reverse

--- code2040.py
+++ code2040.py
@@ -11,5 +11,24 @@ def register():
 	res = requests.post(url, data=data)
 	print(res.text)
 
+def reverse():
+	"""
+	Makes a POST request to http://challenge.code2040.org/api/reverse to obtain
+	a string to reverse, reverses it, and posts the result to
+	http://challenge.code2040.org/api/validate
+	"""
+	url = "http://challenge.code2040.org/api/reverse"
+	data = {"token": "a061df4b77bfec12bc1ec34d70cba02b"}
+	res = requests.post(url, data=data)
+	reverse = res.text[::-1]
+	print(reverse)
+
+	# Post the reversed string
+	validate = "http://challenge.code2040.org/api/reverse/validate"
+	val_data = {"token": "a061df4b77bfec12bc1ec34d70cba02b",\
+				"string": reverse}
+	val_res = requests.post(validate, data=val_data)
+	print(val_res.text)
 
 register()
+reverse()
