913248c5813ad4b97998b7f4368557ae9a8a6419
Carlos Henriquez <carlosh@mit.edu>
2016-11-01 16:37:29 -0400

    Step 3: Needle in a Haystack

--- code2040.py
+++ code2040.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def register():
 	"""
@@ -14,8 +15,8 @@ def register():
 def reverse():
 	"""
 	Makes a POST request to http://challenge.code2040.org/api/reverse to obtain
-	a string to reverse, reverses it, and posts the result to
-	http://challenge.code2040.org/api/validate
+	a string to reverse, reverses it, and POSTs the result to
+	http://challenge.code2040.org/api/reverse/validate
 	"""
 	url = "http://challenge.code2040.org/api/reverse"
 	data = {"token": "a061df4b77bfec12bc1ec34d70cba02b"}
@@ -30,5 +31,26 @@ def reverse():
 	val_res = requests.post(validate, data=val_data)
 	print(val_res.text)
 
+def haystack():
+	"""
+	Makes a POST request to http://challenge.code2040.org/api/haystack to obtain
+	a string and a list, finds the index of the string in the list, and POSTs the
+	result to http://challenge.code2040.org/api/haystack/validate
+	"""
+	url = "http://challenge.code2040.org/api/haystack"
+	data = {"token": "a061df4b77bfec12bc1ec34d70cba02b"}
+	res = requests.post(url, data=data)
+	res_dict = json.loads(res.text)
+	needle, haystack = res_dict['needle'], res_dict['haystack']
+	index = haystack.index(needle) #finds first instance of needle
+	print(haystack, needle, index)
+
+	validate = "http://challenge.code2040.org/api/haystack/validate"
+	val_data = {"token": "a061df4b77bfec12bc1ec34d70cba02b", "needle": index}
+
+	val_res = requests.post(validate, data=val_data)
+	print(val_res.text)
+
 register()
 reverse()
+haystack()
