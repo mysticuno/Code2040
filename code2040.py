a40018adc912f4003a6ccec8133fcb39f6533b2f
Carlos Henriquez <carlosh@mit.edu>
2016-11-02 03:21:58 -0400

    Step 5: Dating

--- code2040.py
+++ code2040.py
@@ -1,5 +1,7 @@
 import requests
 import json
+import datetime
+import dateutil.parser
 
 def register():
 	"""
@@ -22,12 +24,11 @@ def reverse():
 	data = {"token": "a061df4b77bfec12bc1ec34d70cba02b"}
 	res = requests.post(url, data=data)
 	reverse = res.text[::-1]
-	print(reverse)
-
+	
 	# Post the reversed string
 	validate = "http://challenge.code2040.org/api/reverse/validate"
-	val_data = {"token": "a061df4b77bfec12bc1ec34d70cba02b",\
-				"string": reverse}
+	val_data = {"token": "a061df4b77bfec12bc1ec34d70cba02b", "string": reverse}
+	
 	val_res = requests.post(validate, data=val_data)
 	print(val_res.text)
 
@@ -43,9 +44,8 @@ def haystack():
 	
 	res_dict = json.loads(res.text)
 	needle, haystack = res_dict['needle'], res_dict['haystack']
-	index = haystack.index(needle) #finds first instance of needle
-	print(haystack, needle, index)
-
+	index = haystack.index(needle) # finds first instance of needle
+	
 	validate = "http://challenge.code2040.org/api/haystack/validate"
 	val_data = {"token": "a061df4b77bfec12bc1ec34d70cba02b", "needle": index}
 
@@ -65,12 +65,35 @@ def prefix():
 	
 	res_dict = json.loads(res.text)
 	prefix, array = res_dict['prefix'], res_dict['array']
-	print(prefix, array)
-	not_prefix = [string for string in array if not string.startswith(prefix)]
-	print(not_prefix)
 
+	not_prefix = [string for string in array if not string.startswith(prefix)]
+	
 	validate = "http://challenge.code2040.org/api/prefix/validate"
 	val_data = {"token": "a061df4b77bfec12bc1ec34d70cba02b", "array": not_prefix}
+
+	val_res = requests.post(validate, json=val_data)
+	print(val_res.text)
+
+def dating():
+	"""
+	Makes a POST request to http://challenge.code2040.org/api/dating to obtain
+	an ISO 8601 datestamp and a number of seconds, adds the seconds to the
+	datestamp and then POSTs the resulting ISO 8601 datestamp to
+	http://challenge.code2040.org/api/dating/validate
+	"""
+	url = "http://challenge.code2040.org/api/dating"
+	data = {"token": "a061df4b77bfec12bc1ec34d70cba02b"}
+	res = requests.post(url, data=data)
+	
+	res_dict = json.loads(res.text)
+	datestamp, interval = res_dict['datestamp'], res_dict['interval']
+	
+	# Convert to datetime object, the add timedelta
+	dt = dateutil.parser.parse(datestamp, ignoretz=True) + datetime.timedelta(seconds=interval)
+	dt_iso = dt.isoformat() + 'Z'
+
+	validate = "http://challenge.code2040.org/api/dating/validate"
+	val_data = {"token": "a061df4b77bfec12bc1ec34d70cba02b", "datestamp": dt_iso}
 
 	val_res = requests.post(validate, json=val_data)
 	print(val_res.text)
@@ -79,3 +102,4 @@ register()
 reverse()
 haystack()
 prefix()
+dating()
